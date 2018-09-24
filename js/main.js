c7267144774641784260bdb1d1c6b4c15fade719
Susanna Kline <susannakline@gmail.com>
2018-09-24 11:29:26 -0500

    Responsive updates

--- js/main.js
+++ js/main.js
@@ -175,9 +175,12 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
+  more.onClick = function() {
+  const url = DBHelper.urlForRestaurant(restaurant);
+  window.location = url;
+}
   li.append(more)
 
   return li
